885b17880c8ef86945cf78e8579d9940c58a12b9
Yves Talboom <yves.talboom@gmail.com>
2017-11-25 15:01:07 +0100

    Create generateMessage utility

--- server/server.js
+++ server/server.js
@@ -1,7 +1,8 @@
-const path = require("path");
-const http = require("http");
-const express = require("express");
-const socketIO = require("socket.io");
+const path = require('path');
+const http = require('http');
+const express = require('express');
+const socketIO = require('socket.io');
+const { generateMessage } = require('./utils/message');
 
 const publicPath = path.join(`${__dirname}/../public`);
 const port = process.env.PORT || 3000;
@@ -11,28 +12,34 @@ const io = socketIO(server);
 
 app.use(express.static(publicPath));
 
-io.on("connection", socket => {
-  console.log("New user connected");
+io.on('connection', socket => {
+  console.log('New user connected');
 
-  socket.on("createMessage", message => {
-    socket.emit("newMessage", {
-      from: "Admin",
-      text: "Welcome to the chat app",
-      createdAt: new Date().getTime(),
-    });
+  socket.on('createMessage', message => {
+    socket.emit(
+      'newMessage',
+      generateMessage({
+        from: 'Admin',
+        text: 'Welcome to the chat app',
+      }),
+    );
 
-    socket.broadcast.emit("newMessage", {
-      from: "Admin",
-      text: "New user joined",
-      createdAt: new Date().getTime(),
-    });
+    socket.broadcast.emit(
+      'newMessage',
+      generateMessage({
+        from: 'Admin',
+        text: 'New user joined',
+      }),
+    );
 
-    console.log("createMessage:", message);
-    io.emit("newMessage", {
-      from: message.from,
-      text: message.text,
-      createdAt: new Date().getTime(),
-    });
+    console.log('createMessage:', message);
+    io.emit(
+      'newMessage',
+      generateMessage({
+        from: message.from,
+        text: message.text,
+      }),
+    );
     // socket.broadcast.emit("newMessage", {
     //   from: message.from,
     //   text: message.text,
@@ -40,8 +47,8 @@ io.on("connection", socket => {
     // });
   });
 
-  socket.on("disconnect", () => {
-    console.log("disconnected from the server");
+  socket.on('disconnect', () => {
+    console.log('disconnected from the server');
   });
 });
 
